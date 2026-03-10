63b9749fc46e8de96f4d731a583d3ff6bc16c026
Natália de Quadros Caetano <nataliaquadroscaetano@gmail.com>
2026-03-10 14:25:06 -0300

    primeiro commit

---
+++ Arenafut/routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/sobre', function(req, res, next) {
+  res.render('sobre', { title: 'Você esta em Sobre Nós' });
+});
+
+
+
+module.exports = router;
---
+++ biblioteca/routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Natália' });
+});
+
+/* GET home page. */
+router.get('/admin', function(req, res, next) {
+  res.render('index', { title: 'admin' });
+});
+
+
+module.exports = router;
